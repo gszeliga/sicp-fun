1fc043da854ecbb0ef06989d3c154bd51e1b3a94
Guillermo Szeliga <gszeliga@gmail.com>
2018-11-24 18:41:48 +0100

    representing a few data structures

---
+++ representing-queues.scm
@@ -0,0 +1,41 @@
+(define (make-queue)
+  ; ((front-ptr) (rear-ptr))
+  (cons '() '()))
+
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+
+(define (front-ptr q)
+  (car q))
+
+(define (rear-ptr q)
+  (cdr q))
+
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         queue)))
